fa7658068bfe5d84b1ef60e3113eccf6f9f69d88
Ulviyya-R <ulviyyamr@code.edu.az>
2023-03-23 18:30:54 +0400

    This is pronia project

---
+++ P230_Pronia/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using P230_Pronia.DAL;
+using P230_Pronia.Entities;
+using System.Diagnostics;
+
+namespace P230_Pronia.Controllers
+{
+    public class HomeController : Controller
+    {
+        readonly ProniaDbContext _context;
+
+        public HomeController(ProniaDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Slider> slider = _context.Sliders.OrderBy(s=>s.Order).ToList();
+            return View(slider);
+        }
+    }
+}
---
+++ P230_Pronia/Controllers/ShopController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using P230_Pronia.DAL;
+using P230_Pronia.Entities;
+
+namespace P230_Pronia.Controllers
+{
+    public class ShopController:Controller
+    {
+        private readonly ProniaDbContext _context;
+
+        public ShopController(ProniaDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Plant> plants = _context.Plants.Include(p=>p.PlantImages).ToList();
+            return View(plants);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            Plant? plant =_context.Plants.Include(p=>p.PlantCategories).ThenInclude(pc=>pc.Category)
+                .Include(p=>p.PlantTags).ThenInclude(pt=>pt.Tag)
+                .Include(p=>p.PlantDeliveryInformation)
+                .Include(p=>p.PlantImages)
+                .SingleOrDefault(p=>p.Id==id);
+            return View(plant);
+        }
+    }
+}
